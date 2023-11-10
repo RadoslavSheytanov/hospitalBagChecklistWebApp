a538dba738df0e2147bc49631213db7f0f516342
Radoslav Sheytanov <radoslavsheytanov@yahoo.com>
2023-11-10 15:33:38 +0200

    updated functionality, populating the UI

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,117 +1,164 @@
-function setupChecklist(sectionId, addButtonId) {
-    const section = document.getElementById(sectionId);
-  
-    // Function to toggle the background color of the list item
-    const toggleItemBackground = (checkbox) => {
-      checkbox.closest('.list-group-item').classList.toggle('bg-light-green', checkbox.checked);
-    };
-  
-    // Function to create a text span element for the item name
-    const createTextSpan = (itemName) => {
-      const span = document.createElement('span');
-      span.className = 'item-name';
-      span.textContent = itemName;
-      return span;
-    };
-  
-    // Function to create a checkbox input element
-    const createCheckbox = () => {
-      const checkbox = document.createElement('input');
-      checkbox.className = 'form-check-input me-1';
-      checkbox.type = 'checkbox';
-      checkbox.value = '';
-      checkbox.setAttribute('aria-label', '...');
-      checkbox.addEventListener('change', (event) => toggleItemBackground(event.target));
-      return checkbox;
-    };
-  
-    // Function to create a button element
-    const createButton = (text, className, handler) => {
-      const button = document.createElement('button');
-      button.className = `btn btn-sm ${className}`;
-      button.textContent = text;
-      button.addEventListener('click', handler);
-      return button;
-    };
-  
-    // Function to create a new list item with the given item name
-    const createNewItem = (itemName) => {
-      const li = document.createElement('li');
-      li.className = 'list-group-item d-flex justify-content-between align-items-center';
-      
-      const span = createTextSpan(itemName);
-      const checkbox = createCheckbox();
-      const editButton = createButton('Edit', 'btn-outline-secondary editItem', () => editItem(li));
-      const deleteButton = createButton('Delete', 'btn-outline-danger deleteItem', () => deleteItem(li));
-  
-      const div = document.createElement('div');
-      div.className = 'checkboxAndButtons';
-      div.appendChild(checkbox);
-      div.appendChild(editButton);
-      div.appendChild(deleteButton);
-  
-      li.appendChild(span);
-      li.appendChild(div);
-  
-      section.appendChild(li);
-    };
-  
-    // Function to edit the name of an existing list item
-    const editItem = (li) => {
-      const span = li.querySelector('.item-name');
-      const newName = prompt('Edit the item name:', span.textContent);
-      if (newName) {
-        span.textContent = newName;
-      }
-    };
-  
-    // Function to delete an existing list item
-    const deleteItem = (li) => {
-      if (confirm('Are you sure you want to delete this item?')) {
-        section.removeChild(li);
-      }
-    };
-  
-    // Event listener for the 'Add Item' button
-    document.getElementById(addButtonId).addEventListener('click', () => {
-      const itemName = prompt('Enter the name of the new item:');
-      if (itemName) {
-        createNewItem(itemName);
-      }
+console.log('scripts.js loaded');
+
+// Define createNewItem in the global scope
+function createNewItem(section, itemName, id, isChecked) {
+    const li = document.createElement('li');
+    li.className = 'list-group-item d-flex justify-content-between align-items-center';
+    li.setAttribute('data-id', id); // Store the item's ID
+
+    const span = document.createElement('span');
+    span.className = 'item-name';
+    span.textContent = itemName;
+
+    const checkbox = document.createElement('input');
+    checkbox.className = 'form-check-input me-1';
+    checkbox.type = 'checkbox';
+    checkbox.checked = isChecked;
+    checkbox.addEventListener('change', (event) => {
+        const li = event.target.closest('.list-group-item');
+        const id = li.getAttribute('data-id');
+        const checked = event.target.checked ? 1 : 0;
+        updateItemInDatabase(id, checked);
+        event.target.closest('.list-group-item').classList.toggle('bg-light-green', event.target.checked);
     });
-  }
-  
-  // Initial setup once the DOM is fully loaded
-  document.addEventListener('DOMContentLoaded', () => {
-    setupChecklist('mamaSection', 'addMamaItem');
-    setupChecklist('babySection', 'addBabyItem');
-    setupChecklist("breastfeedingSection", "addBreastfeedingItem");
-    setupChecklist('toiletriesSection', 'addtoiletryItem');
-    setupChecklist('usefulTipsSection', 'addAnUsefulTip');
-    setupChecklist('othersSection', 'addOthers');
-  });
 
+    const editButton = document.createElement('button');
+    editButton.className = 'btn btn-sm btn-outline-secondary editItem';
+    editButton.textContent = 'Edit';
+    editButton.addEventListener('click', () => editItem(li));
 
-  // AJAX Communication with the Back-end - Adding items, updating the UI ...
+    const deleteButton = document.createElement('button');
+    deleteButton.className = 'btn btn-sm btn-outline-danger deleteItem';
+    deleteButton.textContent = 'Delete';
+    deleteButton.addEventListener('click', () => deleteItemFromDatabase(id, li));
 
-  function addItemToDatabase(itemName, category) {
+    const div = document.createElement('div');
+    div.className = 'checkboxAndButtons';
+    div.appendChild(checkbox);
+    div.appendChild(editButton);
+    div.appendChild(deleteButton);
+
+    li.appendChild(span);
+    li.appendChild(div);
+
+    section.appendChild(li);
+}
+
+// Define addItemToDatabase in the global scope
+function addItemToDatabase(itemName, category, section) {
     fetch('php/add_item.php', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: `name=${encodeURIComponent(itemName)}&category=${encodeURIComponent(category)}`
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: `name=${encodeURIComponent(itemName)}&category=${encodeURIComponent(category)}`
     })
     .then(response => response.json())
     .then(data => {
-      if (data.id) {
-        console.log('Item added with ID:', data.id);
-        // Add item to the UI list
-      } else {
-        console.error('Error adding item:', data.error);
-      }
+        if (data.id) {
+            createNewItem(section, itemName, data.id, false); // Add the new item to the UI
+        } else {
+            console.error('Error adding item:', data.error);
+        }
     })
-    .catch(error => console.error('Error:', error));
-  }
-  
-  
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
+
+// Define updateItemInDatabase in the global scope
+function updateItemInDatabase(id, checked) {
+    fetch('php/update_item.php', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: `id=${encodeURIComponent(id)}&checked=${encodeURIComponent(checked)}`
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.success) {
+            console.log('Item updated successfully');
+        } else {
+            console.error('Error updating item:', data.error);
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
+
+// Define deleteItemFromDatabase in the global scope
+function deleteItemFromDatabase(id, liElement) {
+    fetch('php/delete_item.php', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: `id=${encodeURIComponent(id)}`
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.success) {
+            liElement.remove(); // Remove the item from the UI
+            console.log('Item deleted successfully');
+        } else {
+            console.error('Error deleting item:', data.error);
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
+
+function editItem(li) {
+    const span = li.querySelector('.item-name');
+    const newName = prompt('Edit the item name:', span.textContent);
+    if (newName) {
+        span.textContent = newName;
+        // update functionality ....
+    }
+}
+
+function setupChecklist(sectionId, addButtonId) {
+    const section = document.getElementById(sectionId);
+    if (!section) {
+        console.error(`Section not found: ${sectionId}`);
+        return;
+    }
+
+    document.getElementById(addButtonId).addEventListener('click', () => {
+        const itemName = prompt('Enter the name of the new item:');
+        if (itemName) {
+            addItemToDatabase(itemName, sectionId.replace('Section', ''), section);
+        }
+    });
+}
+
+function fetchItems() {
+    console.log('Fetching items...');
+    fetch('php/fetch_items.php')
+        .then(response => response.json())
+        .then(items => {
+            console.log('Items fetched:', items);
+            items.forEach(item => {
+                const section = document.getElementById(item.category + 'Section');
+                if (section) {
+                    createNewItem(section, item.name, item.id, item.checked === 1);
+                } else {
+                    console.error('Section not found for category:', item.category);
+                }
+            });
+        })
+        .catch(error => console.error('Error fetching items:', error));
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    setupChecklist('mamaSection', 'addMamaItem');
+    setupChecklist('babySection', 'addBabyItem');
+    setupChecklist('breastfeedingSection', 'addBreastfeedingItem');
+    setupChecklist('toiletriesSection', 'addtoiletryItem');
+    setupChecklist('usefulTipsSection', 'addAnUsefulTip');
+    setupChecklist('othersSection', 'addOthers');
+    fetchItems();
+});
